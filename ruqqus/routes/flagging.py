b939aeca202f946d5f2a273cce7fa036ec1e3be7
Aevann1 <A7.Ibrahim1@gmail.com>
2021-07-12 19:06:02 +0200

    Revert ""
    This reverts commit 8e5b97c78581424e492f7bc192a022e72b647578.

--- ruqqus/routes/flagging.py
+++ ruqqus/routes/flagging.py
@@ -8,12 +8,14 @@ from ruqqus.__main__ import app
 
 
 @app.route("/api/flag/post/<pid>", methods=["POST"])
-@auth_desired
+@is_not_banned
 def api_flag_post(pid, v):
 
 	post = get_post(pid)
 
-	if v:
+	kind = request.form.get("report_type")
+
+	if kind == "admin":
 		existing = g.db.query(Flag).filter_by(
 			user_id=v.id, post_id=post.id).filter(
 			Flag.created_utc >= post.edited_utc).first()
@@ -25,32 +27,45 @@ def api_flag_post(pid, v):
 					user_id=v.id,
 					created_utc=int(time.time())
 					)
-					
 
-		g.db.add(flag)
+	elif kind == "guild":
+		existing = g.db.query(Report).filter_by(
+			user_id=v.id, post_id=post.id).filter(
+			Report.created_utc >= post.edited_utc).first()
+
+		if existing:
+			return "", 409
+
+		flag = Report(post_id=post.id,
+					  user_id=v.id,
+					  created_utc=int(time.time())
+					  )
+	else:
+		return "", 422
+
+	g.db.add(flag)
 
 	return "", 204
 
 
 @app.route("/api/flag/comment/<cid>", methods=["POST"])
-@auth_desired
+@is_not_banned
 def api_flag_comment(cid, v):
 
 	comment = get_comment(cid)
-	
-	if v:
-		existing = g.db.query(CommentFlag).filter_by(
-			user_id=v.id, comment_id=comment.id).filter(
-			CommentFlag.created_utc >= comment.edited_utc).first()
 
-		if existing:
-			return "", 409
+	existing = g.db.query(CommentFlag).filter_by(
+		user_id=v.id, comment_id=comment.id).filter(
+		CommentFlag.created_utc >= comment.edited_utc).first()
 
-		flag = CommentFlag(comment_id=comment.id,
-						   user_id=v.id,
-						   created_utc=int(time.time())
-						   )
+	if existing:
+		return "", 409
 
-		g.db.add(flag)
+	flag = CommentFlag(comment_id=comment.id,
+					   user_id=v.id,
+					   created_utc=int(time.time())
+					   )
+
+	g.db.add(flag)
 
 	return "", 204
