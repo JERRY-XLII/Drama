278dbf66ff57a88dff391ad8a57ce49d98c5522d
Aevann1 <A7.Ibrahim1@gmail.com>
2021-07-16 13:11:28 +0200

    sneed

--- ruqqus/classes/mix_ins.py
+++ ruqqus/classes/mix_ins.py
@@ -22,8 +22,7 @@ class Stndrd:
 	@property
 	@lazy
 	def created_datetime(self):
-		return time.strftime("%d %B %Y at %H:%M:%S",
-							 time.gmtime(self.created_utc))
+		return time.strftime("%d/%B/%Y %H:%M:%S UTC", time.gmtime(self.created_utc))
 
 	@property
 	@lazy
@@ -49,9 +48,7 @@ class Age_times:
 
 	@property
 	def created_datetime(self):
-
-		return time.strftime("%d %b %Y at %H:%M:%S",
-							 time.gmtime(self.created_utc))
+		return time.strftime("%d/%B/%Y %H:%M:%S UTC", time.gmtime(self.created_utc))
 
 	@property
 	def age_string(self):
