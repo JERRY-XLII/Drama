21618fcae606a46d6b0ce435a0be9d57c5037dc0
Aevann1 <m.neumann0001@gmail.com>
2021-07-17 18:49:50 +0200

    sneed

--- ruqqus/classes/user.py
+++ ruqqus/classes/user.py
@@ -439,7 +439,9 @@ class User(Base, Stndrd, Age_times):
 
 	def notification_messagelisting(self, page=1, all_=False):
 
-		notifications = self.notifications.join(Notification.comment).filter(Comment.sentto != None).options(contains_eager(Notification.comment)).all() + g.db.query(Comment).filter_by(author_id=self.id).filter(Comment.parent_submission == None).all()
+		notifications = self.notifications.join(Notification.comment).filter(Comment.sentto != None).options(contains_eager(Notification.comment)).all()
+
+		notifications = [x[0] for x in notifications] + g.db.query(Comment).filter_by(author_id=self.id).filter(Comment.parent_submission == None).all()
 
 		notifications = sorted(notifications, key=lambda x: x.created_utc, reverse=True)
 
