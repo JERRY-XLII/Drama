8d7b5301418bde33a8ae476507373205f113b7fb
Aevann1 <A7.Ibrahim1@gmail.com>
2021-07-17 15:37:06 +0200

    dfdf

--- pushcss.sh
+++ pushcss.sh
@@ -6,7 +6,7 @@ sass ./ruqqus/assets/style/tron.scss D:/#D/ruqqus/assets/style/tron_ff66ac.css
 sass ./ruqqus/assets/style/4chan.scss D:/#D/ruqqus/assets/style/4chan_ff66ac.css
 python ./compilecss.py
 git add .
-git commit -m "" --allow-empty-message
+git commit -m "css"
 git push
 cd D:\1
 git pull
